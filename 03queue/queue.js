d7efc16f63a1002f42778886c62fb8b1565c568a
Antonio Sousa <antssousa@gmail.com>
2022-11-16 21:26:04 -0300

    Implementação de filas

---
+++ 03queue/queue.js
@@ -0,0 +1,103 @@
+function Queue() {
+    let item = []
+
+    // Enfileirar
+    this.enqueue = function (element) {
+        item.push(element)
+        // item[item.length] = element
+    }
+
+    // Desenfileirar
+    this.dequeue = function () {
+        return item.shift()
+    }
+
+    // Olhar o primeiro elemento da fila
+    // Olhar o último elemento da fila
+
+    // Verificar se a fila está vazia
+    this.isEmpty = function () {
+        return item.length === 0
+    }
+
+    // Verificar o tamanho da fila
+    this.size = function () {
+        return item.length
+    }
+
+    // Imprimir a fila
+    this.print = function () {
+        // // for (let i = 0; i < item.length; i++) {
+        // //     if (i < item.length - 1) {
+        // //         res += item[i] + ', '
+        // //     } else {
+        // //         res += item[i]
+        // //     }
+        // // }
+        // // res += ']'
+        // if (this.isEmpty()) {
+        //     console.log('[]')
+        // } else {
+        //     let res = '['
+
+        //     for (let i = 0; i < item.length; i++) {
+        //         res += item[i] + ', '
+        //     }
+        //     // res = res.slice(0, -2) + ']'
+        //     res = res.slice(0, res.length - 2) + ']'
+        //     console.log(res)
+        // }
+        // console.log(item)
+        console.log(`[${item}]`)
+    }
+
+    // Limpar a fila
+    this.clear = function () {
+        item = []
+    }
+
+    // Gerar uma fila clone da atual
+    this.clone = function () {
+        const queue = new Queue()
+        for (let i = 0; i < item.length; i++) {
+            queue.enqueue(item[i])
+        }
+        return queue
+    }
+}
+
+const queue = new Queue()
+console.log(queue.isEmpty())
+queue.enqueue(1)
+console.log(queue.isEmpty())
+queue.print()
+queue.enqueue(2)
+queue.print()
+queue.enqueue(3)
+queue.print()
+queue.enqueue(4)
+queue.print()
+queue.enqueue(5)
+queue.print()
+queue.dequeue()
+queue.print()
+queue.dequeue()
+queue.print()
+queue.dequeue()
+queue.print()
+queue.dequeue()
+queue.print()
+queue.dequeue()
+queue.print()
+queue.dequeue()
+queue.print()
+// Preencher uma fila com valores aleatórios
+for (let i = 0; i < 10; i++) {
+    queue.enqueue(Math.floor(Math.random() * 100))
+}
+queue.print()
+
+// Esvaziar uma fila
+while (!queue.isEmpty()) {
+    console.log(`elemento removido ${queue.dequeue()}`)
+}
---
+++ 03queue/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+ueue.dequeue()
