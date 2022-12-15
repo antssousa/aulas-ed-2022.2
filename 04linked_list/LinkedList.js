ab08b91fc82ed1754954572d563fb58d2b5f3019
Antonio Sousa <antssousa@gmail.com>
2022-12-14 21:28:06 -0300

    Implementação da lista encadeada

--- 04linked_list/LinkedList.js
+++ 04linked_list/LinkedList.js
@@ -28,13 +28,13 @@ function LinkedList() {
     this.insert = function (index, element) {
         if (index >= 0 && index <= length) {
             let node = new Node(element)
-            if (this.isEmpty()) {
+            if (index == 0) {
+                node.next = head
                 head = node
                 length++
             } else {
                 let i = 0
                 let current = head
-                console.log(current.element)
                 while (i < index - 1) {
                     current = current.next
                     i++
@@ -49,19 +49,95 @@ function LinkedList() {
     }
 
     // Remover um elemento da posição passada
-    this.removeAt = function (index) {}
+    this.removeAt = function (index) {
+        if (index >= 0 && index < length) {
+            if (index === 0) {
+                let remove = head
+                head = head.next
+                length--
+                return remove.element
+            } else if (index === length - 1) {
+                let current = head
+                let previous = null
+                while (current.next) {
+                    previous = current
+                    current = current.next
+                }
+                let remove = current
+                previous.next = null
+                length--
+                return remove.element
+            } else {
+                let i = 0
+                let previous = null
+                let current = head
+                while (i < index) {
+                    previous = current
+                    current = current.next
+                    i++
+                }
+
+                let remove = current
+                previous.next = current.next
+                length--
+                return remove.element
+            }
+        } else {
+            console.log('Indice incorreto')
+        }
+    }
 
     // Remove o elemento passado como parâmetro
-    this.remove = function (element) {}
+    this.remove = function (element) {
+        let index = this.indexOf(element)
+        if (index !== -1) {
+            return this.removeAt(index)
+        } else {
+            console.log('Elemento nao existe na lista')
+        }
+    }
 
     // Retorna o índice do primeiro elemento recebido como parâmetro
-    this.indexOf = function (element) {}
+    this.indexOf = function (element) {
+        if (this.isEmpty()) {
+            return -1
+        } else {
+            let current = head
+            let index = 0
+            while (current) {
+                if (current.element === element) {
+                    return index
+                }
+                index++
+                current = current.next
+            }
+            return -1
+        }
+    }
 
     // Retorna o índice do último elemento recebido como parâmetro
-    this.lastIndexOf = function (element) {}
+    this.lastIndexOf = function (element) {
+        if (this.isEmpty()) {
+            return -1
+        } else {
+            let index = -1
+            let current = head
+            let i = 0
+            while (current) {
+                if (current.element === element) {
+                    index = i
+                }
+                i++
+                current = current.next
+            }
+            return index
+        }
+    }
 
     // Retorna verdadeiro se o elemento está na linked list
-    this.contains = function (element) {}
+    this.contains = function (element) {
+        return this.indexOf(element) !== -1
+    }
 
     // Retorna verdadeiro se a linked list estiver vazia
     this.isEmpty = function () {
@@ -74,7 +150,19 @@ function LinkedList() {
     }
 
     // Retorna o elemento do índice passado como parâmetro
-    this.get = function (index) {}
+    this.get = function (index) {
+        if (index >= 0 && index < length) {
+            let current = head
+            let i = 0
+            while (i < index) {
+                current = current.next
+                i++
+            }
+            return current.element
+        } else {
+            console.log('Indice invalido')
+        }
+    }
 
     // Imprime a linked list
     this.print = function () {
@@ -93,10 +181,21 @@ function LinkedList() {
     }
 
     // Apaga a linked list
-    this.clear = function () {}
+    this.clear = function () {
+        head = null
+        length = 0
+    }
 
     // Converte a linked list num array
-    this.toArray = function () {}
+    this.toArray = function () {
+        let array = []
+        let current = head
+        while (current) {
+            array.push(current.element)
+            current = current.next
+        }
+        return array
+    }
 }
 
 const ll = new LinkedList()
@@ -114,3 +213,40 @@ ll.insert(2, 200)
 ll.print()
 ll.insert(3, 400)
 ll.print()
+ll.print()
+ll.insert(ll.size(), 500)
+ll.print()
+ll.insert(0, 500)
+ll.print()
+console.log(ll.removeAt(0))
+ll.print()
+console.log('>' + ll.removeAt(3))
+ll.print()
+console.log('>' + ll.removeAt(2))
+ll.print()
+console.log('>' + ll.removeAt(1))
+ll.print()
+console.log('>' + ll.removeAt(ll.size() - 1))
+ll.print()
+ll.append(5)
+ll.append(6)
+ll.append(7)
+ll.append(8)
+ll.print()
+console.log(ll.indexOf(1))
+console.log(ll.indexOf(5))
+console.log(ll.indexOf(8))
+console.log(ll.remove(1))
+console.log(ll.remove(5))
+console.log(ll.remove(8))
+console.log(ll.remove(9))
+ll.print()
+ll.append(4)
+ll.print()
+console.log(ll.indexOf(4))
+console.log(ll.lastIndexOf(4))
+console.log(ll.contains(7))
+console.log(ll.get(2))
+ll.print()
+let array = ll.toArray()
+console.log(array)
